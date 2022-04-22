b9305e0f0a7c4a9f85d2ca2db5e5ab201386245a
ugur sag <ug.sagsoz@gmail.com>
2022-04-22 10:36:03 +0100

    review coding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import 'flowbite';
 import { useState, } from 'react';
 
 
-
 interface IState{
   image: string
   description: string,
@@ -19,7 +18,7 @@ interface IState{
 
 function IndexPage() {
 
-const [recipes, setpeople] = useState<IState[]>([
+const [recipes, setrecipes] = useState<IState[]>([
   {
     image: 'https://i.ibb.co/SvkwTdw/asd.png',
     description: 'Portobello Baked Eggs with avocado&bread',
@@ -39,29 +38,36 @@ const [recipes, setpeople] = useState<IState[]>([
 return (
   <Layout>
     <Seo title="Home" />
-    <div><div className="row asd">
-    <div className="aboutwidh about-content-backround">
+    <div>
+      <div className="row asd">
+        <div className="aboutwidh about-content-backround">
           <div className="about-content">
-         <div className="text1">
-           <h4 className=" text-4xl font-bold">You deserve better</h4>
-         </div>
-       <div className="text2">
-        <p className="text-2xl ">Consider this a</p>
-        <p className="text-2xl ">taste information</p>
-       </div>
-      
-       <button className="bg-green-400 text-black font-bold py-2 px-4 rounded"> Learn More</button>
-    </div>
-    </div>
-      <div className="aboutimage col-lg-6">
-        <div className="about-img">
-          <img src="https://i.ibb.co/rvQQWLS/tailwind.png" className="img-fluid w-100 rounded" alt=""/>
+            <div className="text1">
+              <h4 className=" text-4xl font-bold">You deserve better</h4>
+            </div>
+            <div className="text2">
+              <p className="text-2xl ">Consider this a</p>
+              <p className="text-2xl ">taste information</p>
+            </div>
+            <button className="bg-green-400 text-black font-bold py-2 px-4 rounded">
+              {" "}
+              Learn More
+            </button>
           </div>
-          </div>   
-   </div>
-  </div>
- <LastRecipes recipes={recipes}/>
+        </div>
+        <div className="aboutimage col-lg-6">
+          <div className="about-img">
+            <img
+              src="https://i.ibb.co/rvQQWLS/tailwind.png"
+              className="img-fluid w-100 rounded"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <LastRecipes recipes={recipes} />
   </Layout>
-);
+)
 }
 export default IndexPage
